fc2c5441f7d645bb688c545007649607bdab049f
Nick Flint <nick.r.flint@gmail.com>
2021-09-16 07:20:19 -0400

    add explicit table name

--- src/main/java/com/simulationhockey/columbus/scout_api/api/userinformation/UserInformation.java
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/userinformation/UserInformation.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="user_information")
 public class UserInformation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
