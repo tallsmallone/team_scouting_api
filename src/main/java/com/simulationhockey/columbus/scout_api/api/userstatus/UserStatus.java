c2ba7a2bcd2e7729f36225e816393825d6f6345b
Nick Flint <nick.r.flint@gmail.com>
2021-09-16 08:38:13 -0400

    add user status table

---
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/userstatus/UserStatus.java
@@ -0,0 +1,62 @@
+package com.simulationhockey.columbus.scout_api.api.userstatus;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_status")
+public class UserStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Integer user_id;
+    private Boolean want;
+    private Boolean avoid;
+    private Boolean drafted;
+
+    // Getters
+    public Integer getId(){
+        return this.id;
+    }
+
+    public Integer getUserId(){
+        return this.user_id;
+    }
+
+    public Boolean getWant(){
+        return this.want;
+    }
+
+    public Boolean getAvoid(){
+        return this.avoid;
+    }
+
+    public Boolean getDrafted(){
+        return this.drafted;
+    } 
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUserId(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setWant(Boolean want) {
+        this.want = want;
+    }
+
+    public void setAvoid(Boolean avoid) {
+        this.avoid = avoid;
+    }
+
+    public void setDrafted(Boolean drafted) {
+        this.drafted = drafted;
+    }
+}
---
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/userstatus/UserStatusRepository.java
@@ -0,0 +1,6 @@
+package com.simulationhockey.columbus.scout_api.api.userstatus;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserStatusRepository extends CrudRepository<UserStatus, Integer>{
+}
