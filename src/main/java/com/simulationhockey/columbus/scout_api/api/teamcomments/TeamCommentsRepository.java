da236995ba6afb919e6a2da2555a9d65af19e6b6
Nick Flint <nick.r.flint@gmail.com>
2021-09-16 09:42:54 -0400

    update team comments to use List type

--- src/main/java/com/simulationhockey/columbus/scout_api/api/teamcomments/TeamCommentsRepository.java
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/teamcomments/TeamCommentsRepository.java
@@ -1,8 +1,10 @@
 package com.simulationhockey.columbus.scout_api.api.teamcomments;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TeamCommentsRepository extends CrudRepository<TeamComments, Integer>{
-    Iterable<TeamComments> findByUserId(Integer id);
-    Iterable<TeamComments> findByCommenterId(Integer id);
+    List<TeamComments> findByUserId(Integer id);
+    List<TeamComments> findByCommenterId(Integer id);
 }
