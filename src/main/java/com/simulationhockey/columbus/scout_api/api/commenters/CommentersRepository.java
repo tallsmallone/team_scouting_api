6db20046a6e72f66b43c0ee59006ffbdc3f49b28
Nick Flint <nick.r.flint@gmail.com>
2021-09-16 10:10:44 -0400

    add ability to search by username

--- src/main/java/com/simulationhockey/columbus/scout_api/api/commenters/CommentersRepository.java
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/commenters/CommentersRepository.java
@@ -1,6 +1,9 @@
 package com.simulationhockey.columbus.scout_api.api.commenters;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentersRepository extends CrudRepository<Commenters, Integer>{
+    List<Commenters> findByUsername(String username);
 }
