096efe274e479517bad6a0c49e5296a14272a004
Nick Flint <nick.r.flint@gmail.com>
2021-09-16 06:18:48 -0400

    initial commit

---
+++ src/test/java/com/simulationhockey/columbus/scout_api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulationhockey.columbus.scout_api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
