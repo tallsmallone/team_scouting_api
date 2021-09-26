8338247c6031ac7d611b467fea47da290fb936b0
Nick Flint <nick.r.flint@gmail.com>
2021-09-26 10:59:13 -0400

    added cors exception for the angular dev application.

--- src/main/java/com/simulationhockey/columbus/scout_api/api/ApiApplication.java
+++ src/main/java/com/simulationhockey/columbus/scout_api/api/ApiApplication.java
@@ -19,6 +19,7 @@ public class ApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
